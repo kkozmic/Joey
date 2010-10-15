0adb2b6a6bff2131596b811bf88fb207e84e72e9
dtchepak <dave@davesquared.net>
2010-10-15 23:34:35 +1100

    Adding test task to run xunit tests, and add to default target
    Hardcoding framework version for msbuild path. Can grab from registry if required.

--- rakefile.rb
+++ rakefile.rb
@@ -1,12 +1,22 @@
 require 'rake'
 require 'albacore'
 
-task :default => [:compile]
+DOT_NET_PATH = "#{ENV["SystemRoot"]}\\Microsoft.NET\\Framework\\v4.0.30319"
+
+ENV["config"] = "Debug" if ENV["config"].nil?
+CONFIG = ENV["config"]
+
+task :default => [:compile, :test]
 
 desc "Compile solution"
 msbuild :compile do |msb|
-    msb.properties :configuration => :Debug
+    msb.properties :configuration => CONFIG
+    msb.path_to_command = File.join(DOT_NET_PATH, "msbuild.exe")
     msb.targets :Clean, :Build
     msb.solution = "Joey.sln"
 end
 
+xunit :test => [:compile] do |xunit|
+    xunit.path_to_command = "lib/xunit.console.clr4.exe"
+    xunit.assembly = "tst/Joey.Tests/bin/#{CONFIG}/Joey.Tests.dll"
+end
