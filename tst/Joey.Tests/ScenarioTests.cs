27c06e2613802ffdc68000b6358f9d70106cbab0
Krzysztof Kozmic <krzysztof@kozmic.pl>
2010-10-15 22:00:51 +1000

    - some actual code - at last!

---
+++ tst/Joey.Tests/ScenarioTests.cs
@@ -0,0 +1,39 @@
+// Copyright (C) 2010, Krzysztof Kozmic 
+// 
+// Permission is hereby granted, free of charge, to any person obtaining a 
+// copy of this software and associated documentation files (the "Software"), 
+// to deal in the Software without restriction, including without limitation 
+// the rights to use, copy, modify, merge, publish, distribute, sublicense, 
+// and/or sell copies of the Software, and to permit persons to whom the 
+// Software is furnished to do so, subject to the following conditions: 
+// 
+// The above copyright notice and this permission notice shall be included in 
+// all copies or substantial portions of the Software. 
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR 
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL 
+// THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER 
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING 
+// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER 
+// DEALINGS IN THE SOFTWARE. 
+
+namespace Joey.Tests
+{
+	using Joey.Tests.Meta;
+
+	using Xunit;
+	using Xunit.Sdk;
+
+	public class ScenarioTests
+	{
+		[Fact]
+		public void Scenario_is_recognized_as_test_method()
+		{
+			var scenario = typeof(HasSingleScenario).GetMethod("ScenarioMethod");
+			Assert.NotNull(scenario);
+			var methodInfo = Reflector.Wrap(scenario);
+			Assert.True(MethodUtility.IsTest(methodInfo));
+		}
+	}
+}
