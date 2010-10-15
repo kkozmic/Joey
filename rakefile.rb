3c94c06d5f50510ca0b42d7c95ec13338a40d233
dtchepak <dave@davesquared.net>
2010-10-15 21:19:03 +0800

    - Added copy_to_output task - Added clean task to clean ./out path

--- rakefile.rb
+++ rakefile.rb
@@ -1,14 +1,19 @@
 require 'rake'
+require 'rake/clean'
 require 'albacore'
+require 'fileutils'
 
 DOT_NET_PATH = "#{ENV["SystemRoot"]}\\Microsoft.NET\\Framework\\v4.0.30319"
+OUTPUT_PATH = "out"
 
 ENV["config"] = "Debug" if ENV["config"].nil?
 CONFIG = ENV["config"]
 
-task :default => [:compile, :test]
+CLEAN.include(OUTPUT_PATH)
 
-desc "Compile solution"
+desc "Full compile and test"
+task :default => [:clean, :compile, :test, :copy_to_output]
+
 msbuild :compile do |msb|
     msb.properties :configuration => CONFIG
     msb.path_to_command = File.join(DOT_NET_PATH, "msbuild.exe")
@@ -19,4 +24,9 @@ end
 xunit :test => [:compile] do |xunit|
     xunit.path_to_command = "lib/xunit.console.clr4.exe"
     xunit.assembly = "tst/Joey.Tests/bin/#{CONFIG}/Joey.Tests.dll"
+end
+
+task :copy_to_output => [:compile] do
+    build_artefacts = FileList.new("src/**/#{CONFIG}/.").exclude("/obj/")
+    FileUtils.cp_r build_artefacts, "#{OUTPUT_PATH}/#{CONFIG}/"
 end
